ef0c10d34a7e96a3929951ce1bca9b86abc00df9
umackr <leobarros_15b@hotmail.com>
2021-10-19 20:46:44 -0300

    adicionando GitIgnore e arquivo principal

---
+++ TrabalhoN1pt2.py
@@ -0,0 +1,12 @@
+from io import open_code
+import os
+from typing import Optional
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+from time import sleep
+
+def main():
+    print("Teste")
+
+
+if __name__ == "__main__":
+    main()
