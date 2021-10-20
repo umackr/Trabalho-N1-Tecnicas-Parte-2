08802517b3096c002a82bde58824caa882928975
Paulo Machado Fraga Matos <paulo.machado.fraga.matos@gmail.com>
2021-10-19 22:13:16 -0300

    Adicionando função de remover o usuario

--- TrabalhoN1pt2.py
+++ TrabalhoN1pt2.py
@@ -43,6 +43,37 @@ def verificarUsuario(listaDeUsuarios):
         input("\nPressione ENTER para continuar")
     clearConsole()
 
+def removerUsuario(listaDeUsuarios):
+    email = str(input("Insira o e-mail do usuario que você gostaria de remover?\n"))
+    clearConsole()
+
+    while True:
+        for usuario in listaDeUsuarios:
+
+            if listaDeUsuarios[usuario] == email:
+                print("NOME:",usuario,"\tE-MAIL:",listaDeUsuarios[usuario],"\n")
+                escolha = int(input("Você realmente quer deletar o usuario acima? \n1 - Sim \n2 - Não\n"))
+                clearConsole()
+
+                if escolha == 1:
+                    del listaDeUsuarios[usuario] 
+                    print("Usuario deletado com sucesso!\n")
+                    sleep(1)
+                    clearConsole()
+                    return
+                elif escolha == 2:
+                    escolha = input("1 - Remover outro usuario \n\nPressione ENTER para sair\n")
+                    if escolha == "1":
+                        clearConsole()
+                        return removerUsuario(listaDeUsuarios)
+                    else: 
+                        clearConsole()
+                        return
+                else: print("Valor invalido, tente novamente\n")
+            else:print("E-MAIL não cadastrado")
+        sleep(2)
+        clearConsole()
+
 def menu():
     print("-------------MENU---------------")
     print("1 - Cadastrar usuario")
@@ -65,9 +96,11 @@ def main():
         elif opçao == 2: exibirUsuarios(listaDeUsuarios)
         elif opçao == 3: exibirPorNome(listaDeUsuarios)
         elif opçao == 4: verificarUsuario(listaDeUsuarios)
-        elif opçao == 5: print("teste")
+        elif opçao == 5: removerUsuario(listaDeUsuarios)
         elif opçao == 6: print("teste")
         elif opçao == 7: return
+
+
 
 
 if __name__ == "__main__":
