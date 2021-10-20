2f91d652b7d1a7592b735ba1adff56a7bdb99302
b1elzin <knightplay4@gmail.com>
2021-10-19 21:47:44 -0300

    Adicionando cadastro e exibição de usuários

--- TrabalhoN1pt2.py
+++ TrabalhoN1pt2.py
@@ -4,6 +4,24 @@ from typing import Optional
 clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 from time import sleep
 
+def cadastrarUsuario(listaDeUsuarios):
+    nome = str(input("Insira o nome do usuario.\n")).lower()
+    email = str(input("\nInsira o e-mail do usuario.\n")).lower()
+
+    listaDeUsuarios[nome] = email
+    clearConsole()
+    print("Usuario cadastrado com sucesso!")
+    sleep(1)
+    clearConsole()
+    return listaDeUsuarios
+
+def exibirUsuarios(listaDeUsuarios):
+    print("Usuarios cadastrados (Ordem de Cadastro):\n")
+    for usuarios in listaDeUsuarios:
+        print("NOME:",usuarios,"\tE-MAIL:",listaDeUsuarios[usuarios])
+    input("\nPressione ENTER para continuar")
+    clearConsole()
+
 def menu():
     print("-------------MENU---------------")
     print("1 - Cadastrar usuario")
@@ -22,8 +40,8 @@ def main():
         menu()
         opçao = int(input("-------Escolha uma opção--------\n"))
         clearConsole()
-        if opçao == 1: print("teste")
-        elif opçao == 2: print("teste")
+        if opçao == 1: cadastrarUsuario(listaDeUsuarios)
+        elif opçao == 2: exibirUsuarios(listaDeUsuarios)
         elif opçao == 3: print("teste")
         elif opçao == 4: print("teste")
         elif opçao == 5: print("teste")
